f69e9e96d518c824e08a147e32578edf67cbc3d1
Emanuele Curati <ema@outlook.com>
2019-10-10 19:37:07 +0200

    Compatibility v2

--- up_integration-test-env.ps1
+++ up_integration-test-env.ps1
@@ -3,8 +3,8 @@
 Restart-Service docker
 
 # Up containers
-docker-compose -f docker-compose.ci.yml pull
-docker-compose -f --compatibility docker-compose.ci.yml up -d
+docker-compose --compatibility -f docker-compose.ci.yml pull
+docker-compose --compatibility -f docker-compose.ci.yml up -d
 
 # Install curl
 cinst curl -y --no-progress
